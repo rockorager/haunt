b010bbcc38e0728276328e72eb415d1cffceb178
Tim Culverhouse <tim@timculverhouse.com>
2025-01-18 18:58:20 -0600

    implement renderer

--- src/haunt.zig
+++ src/haunt.zig
@@ -3,11 +3,13 @@ const ghostty = @import("ghostty");
 const vaxis = @import("vaxis");
 
 const CoreApp = ghostty.App;
+const CoreSurface = ghostty.Surface;
 const apprt = ghostty.apprt;
 
 const log = std.log.scoped(.haunt);
 
 const Event = union(enum) {
+    wakeup,
     key_press: vaxis.Key,
     winsize: vaxis.Winsize,
 };
@@ -16,8 +18,14 @@ pub const App = struct {
     core: *CoreApp,
     vx: vaxis.Vaxis,
     tty: vaxis.Tty,
+    config: ghostty.config.Config,
+    loop: ?vaxis.Loop(Event),
+
+    surfaces: std.ArrayList(*Surface),
 
     opts: Options,
+
+    size: vaxis.Winsize,
 
     pub const Options = struct {};
 
@@ -27,6 +35,15 @@ pub const App = struct {
             .vx = try vaxis.init(core_app.alloc, .{}),
             .tty = try vaxis.Tty.init(),
             .opts = opts,
+            .config = try ghostty.config.Config.load(core_app.alloc),
+            .loop = null,
+            .surfaces = std.ArrayList(*Surface).init(core_app.alloc),
+            .size = .{
+                .rows = 0,
+                .cols = 0,
+                .x_pixel = 0,
+                .y_pixel = 0,
+            },
         };
     }
 
@@ -35,10 +52,11 @@ pub const App = struct {
         const tty = &self.tty;
         const gpa = self.core.alloc;
 
-        var loop: vaxis.Loop(Event) = .{
+        self.loop = .{
             .tty = tty,
             .vaxis = vx,
         };
+        const loop = &self.loop.?;
         try loop.init();
 
         try loop.start();
@@ -46,6 +64,11 @@ pub const App = struct {
 
         try vx.enterAltScreen(tty.anyWriter());
         try vx.queryTerminal(tty.anyWriter(), 1 * std.time.ns_per_s);
+
+        // Queue a new window to create a launch
+        _ = self.core.mailbox.push(.{
+            .new_window = .{},
+        }, .{ .forever = {} });
 
         while (true) {
             loop.pollEvent();
@@ -59,7 +82,10 @@ pub const App = struct {
                     },
                     .winsize => |ws| {
                         try vx.resize(gpa, tty.anyWriter(), ws);
+                        self.size = ws;
+                        // TODO: update surfaces
                     },
+                    .wakeup => {},
                 }
             }
 
@@ -68,8 +94,13 @@ pub const App = struct {
     }
 
     pub fn terminate(self: *App) void {
+        for (self.surfaces.items) |surface| {
+            surface.close(false);
+        }
+        self.surfaces.deinit();
         self.vx.deinit(self.core.alloc, self.tty.anyWriter());
         self.tty.deinit();
+        self.config.deinit();
     }
 
     pub fn performAction(
@@ -78,9 +109,21 @@ pub const App = struct {
         comptime action: apprt.Action.Key,
         value: apprt.Action.Value(action),
     ) !void {
+        log.warn("action={s}", .{@tagName(action)});
+        switch (action) {
+            .new_window => {
+                const parent: ?*CoreSurface = switch (target) {
+                    .app => null,
+                    .surface => |v| v,
+                };
+                // TODO: should we ignore the return?
+                const surface = try self.newSurface(parent);
+                try surface.setSize(self.size);
+                try self.surfaces.append(surface);
+            },
+            else => {},
+        }
         _ = value; // autofix
-        _ = target; // autofix
-        _ = self; // autofix
     }
 
     pub fn redrawInspector(_: App, _: *Surface) void {
@@ -92,6 +135,21 @@ pub const App = struct {
         _ = self; // autofix
         // TODO:
     }
+
+    fn newSurface(self: *App, _: ?*CoreSurface) !*Surface {
+        var surface = try self.core.alloc.create(Surface);
+        errdefer self.core.alloc.destroy(surface);
+
+        try surface.init(self);
+        return surface;
+    }
+
+    pub fn wakeup(self: *App) void {
+        log.debug("{s}", .{@src().fn_name});
+        if (self.loop) |*loop| {
+            loop.postEvent(.wakeup);
+        }
+    }
 };
 
 pub const Surface = struct {
@@ -100,8 +158,34 @@ pub const Surface = struct {
     title: [:0]const u8 = "",
     app: *App,
 
+    pixel_size: apprt.SurfaceSize,
+    cell_size: struct {
+        width: u16,
+        height: u16,
+    },
+
+    pub fn init(self: *Surface, app: *App) !void {
+        self.app = app;
+        self.pixel_size = .{
+            // TODO: when do set this?
+            .width = 100,
+            .height = 100,
+        };
+        self.cell_size.width = 10;
+        self.cell_size.height = 10;
+
+        var config = try apprt.surface.newConfig(app.core, &app.config);
+        defer config.deinit();
+
+        try app.core.addSurface(self);
+        errdefer app.core.deleteSurface(self);
+
+        try self.core_surface.init(app.core.alloc, &config, app.core, app, self);
+    }
+
     pub fn deinit(self: *Surface) void {
-        _ = self;
+        self.app.core.deleteSurface(self);
+        self.core_surface.deinit();
     }
 
     pub fn shouldClose(self: Surface) bool {
@@ -110,8 +194,10 @@ pub const Surface = struct {
 
     pub fn close(self: *Surface, process_active: bool) void {
         // TODO:
-        _ = self;
         _ = process_active;
+        self.should_close = true;
+        self.deinit();
+        self.app.core.alloc.destroy(self);
     }
 
     pub fn getContentScale(_: Surface) !apprt.ContentScale {
@@ -144,5 +230,16 @@ pub const Surface = struct {
         const tty = &self.app.tty;
         const vx = &self.app.vx;
         try vx.copyToSystemClipboard(tty.anyWriter(), std.mem.span(val.ptr), self.app.core.alloc);
+    }
+
+    /// Returns the pixel size of the screen
+    pub fn getSize(self: Surface) !apprt.SurfaceSize {
+        return self.pixel_size;
+    }
+
+    fn setSize(self: *Surface, size: vaxis.Winsize) !void {
+        self.pixel_size.height = size.y_pixel;
+        self.pixel_size.width = size.x_pixel;
+        try self.core_surface.sizeCallback(self.pixel_size);
     }
 };
--- src/renderer.zig
+++ src/renderer.zig
@@ -1,7 +1,26 @@
 const std = @import("std");
 const ghostty = @import("ghostty");
+const vaxis = @import("vaxis");
+
+const haunt = @import("haunt.zig");
+
+const apprt = ghostty.apprt;
+const renderer = ghostty.renderer;
+const terminal = ghostty.terminal;
+
+const log = std.log.scoped(.vaxis_renderer);
 
 pub const Vaxis = struct {
+    gpa: std.mem.Allocator,
+
+    surface_mailbox: apprt.surface.Mailbox,
+
+    foreground_color: ?terminal.color.RGB,
+    background_color: ?terminal.color.RGB,
+    cursor_color: ?terminal.color.RGB,
+
+    dirty: bool,
+
     pub const DerivedConfig = struct {
         pub fn init(gpa: std.mem.Allocator, config: *const ghostty.config.Config) !DerivedConfig {
             _ = gpa;
@@ -13,4 +32,166 @@ pub const Vaxis = struct {
             _ = self;
         }
     };
+
+    pub fn init(gpa: std.mem.Allocator, options: renderer.Options) !Vaxis {
+        return .{
+            .gpa = gpa,
+            .surface_mailbox = options.surface_mailbox,
+            .foreground_color = null,
+            .background_color = null,
+            .cursor_color = null,
+            .dirty = false,
+        };
+    }
+
+    pub fn deinit(self: *Vaxis) void {
+        _ = self;
+    }
+
+    pub fn surfaceInit(_: *apprt.Surface) !void {
+        log.debug("{s}", .{@src().fn_name});
+    }
+
+    pub fn finalizeSurfaceInit(self: *Vaxis, surface: *haunt.Surface) !void {
+        _ = self;
+        _ = surface;
+        log.debug("{s}", .{@src().fn_name});
+    }
+
+    pub fn threadEnter(self: *Vaxis, surface: *haunt.Surface) !void {
+        log.debug("{s}", .{@src().fn_name});
+        _ = self;
+        _ = surface;
+    }
+
+    pub fn threadExit(self: *Vaxis, surface: *haunt.Surface) void {
+        log.debug("{s}", .{@src().fn_name});
+        _ = self;
+        _ = surface;
+    }
+
+    pub fn setVisible(self: *Vaxis, visible: bool) void {
+        log.debug("{s}", .{@src().fn_name});
+        _ = visible; // autofix
+        _ = self; // autofix
+    }
+
+    pub fn setFocus(self: *Vaxis, focused: bool) !void {
+        log.debug("{s}", .{@src().fn_name});
+        _ = focused; // autofix
+        _ = self; // autofix
+    }
+
+    pub fn hasVsync(self: *Vaxis) bool {
+        _ = self; // autofix
+        log.debug("{s}", .{@src().fn_name});
+        return false;
+    }
+
+    pub fn drawFrame(self: *Vaxis, surface: *haunt.Surface) !void {
+        log.debug("{s}", .{@src().fn_name});
+        _ = self;
+        _ = surface;
+    }
+
+    pub fn setFontGrid(self: *Vaxis, grid: anytype) void {
+        _ = grid; // autofix
+        _ = self; // autofix
+        log.debug("{s}", .{@src().fn_name});
+    }
+
+    pub fn markDirty(self: *Vaxis) void {
+        self.dirty = true;
+    }
+
+    pub fn setScreenSize(self: *Vaxis, size: renderer.Size) !void {
+        _ = size; // autofix
+        _ = self; // autofix
+        log.debug("{s}", .{@src().fn_name});
+    }
+
+    pub fn changeConfig(self: *Vaxis, config: *DerivedConfig) !void {
+        _ = config; // autofix
+        _ = self; // autofix
+        log.debug("{s}", .{@src().fn_name});
+    }
+
+    pub fn updateFrame(
+        self: *Vaxis,
+        surface: *apprt.Surface,
+        state: *renderer.State,
+        cursor_blink_visible: bool,
+    ) !void {
+        const vx = &surface.app.vx;
+        const win = vx.window();
+
+        var arena = std.heap.ArenaAllocator.init(self.gpa);
+        defer arena.deinit();
+        const allocator = arena.allocator();
+
+        const screen = state.terminal.screen;
+        var row_iter = screen.pages.rowIterator(.right_down, .{ .viewport = .{} }, null);
+        var row: u16 = 0;
+        while (row_iter.next()) |pin| {
+            defer row += 1;
+            var col: u16 = 0;
+            const cells = pin.cells(.all);
+            for (cells) |cell| {
+                const vx_style = ghosttyStyleToVaxisStyle(pin.style(&cell));
+                if (cell.hasText()) {
+                    var buf: [16]u8 = undefined;
+                    const n = try std.unicode.utf8Encode(cell.codepoint(), &buf);
+                    const char = try allocator.dupe(u8, buf[0..n]);
+                    const width = win.gwidth(char);
+                    defer col += width;
+                    win.writeCell(col, row, .{
+                        .char = .{
+                            .grapheme = char,
+                            .width = @intCast(width),
+                        },
+                        .style = vx_style,
+                    });
+
+                    log.debug("{s}", .{buf[0..n]});
+                }
+            }
+        }
+
+        win.showCursor(state.terminal.screen.cursor.x, state.terminal.screen.cursor.y);
+
+        try vx.render(surface.app.tty.anyWriter());
+        _ = cursor_blink_visible; // autofix
+        log.debug("{s}", .{@src().fn_name});
+    }
 };
+
+fn ghosttyStyleToVaxisStyle(style: ghostty.terminal.Style) vaxis.Style {
+    const fg: vaxis.Color = switch (style.fg_color) {
+        .none => .default,
+        .palette => |v| .{ .index = v },
+        .rgb => |v| .{ .rgb = .{ v.r, v.g, v.b } },
+    };
+    const bg: vaxis.Color = switch (style.bg_color) {
+        .none => .default,
+        .palette => |v| .{ .index = v },
+        .rgb => |v| .{ .rgb = .{ v.r, v.g, v.b } },
+    };
+    const ul: vaxis.Color = switch (style.underline_color) {
+        .none => .default,
+        .palette => |v| .{ .index = v },
+        .rgb => |v| .{ .rgb = .{ v.r, v.g, v.b } },
+    };
+    return .{
+        .fg = fg,
+        .bg = bg,
+        .ul = ul,
+        .ul_style = @enumFromInt(@intFromEnum(style.flags.underline)),
+        .bold = style.flags.bold,
+        .dim = style.flags.faint,
+        .italic = style.flags.italic,
+        .blink = style.flags.blink,
+        .reverse = style.flags.inverse,
+        .invisible = style.flags.invisible,
+        .strikethrough = style.flags.strikethrough,
+    };
+}
